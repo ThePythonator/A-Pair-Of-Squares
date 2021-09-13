5da37f0e6affb6aec7438fa479130f774a302ff5
ThePythonator <49040244+ThePythonator@users.noreply.github.com>
2021-09-13 15:28:58 +0100

    Trying to fix github actions.

--- SDL2/sdl2_image-config.cmake
+++ SDL2/sdl2_image-config.cmake
@@ -1,10 +1,17 @@
-set(SDL_IMAGE_INCLUDE_DIRS "${CMAKE_CURRENT_LIST_DIR}/include")
+find_path(SDL2_IMAGE_INCLUDE_DIR SDL_image.h
+	HINTS ${SDL2_DIR}
+	PATH_SUFFIXES SDL2 include/SDL2 include
+)
 
-# Support both 32 and 64 bit builds
-if (${CMAKE_SIZEOF_VOID_P} MATCHES 8)
-  set(SDL2_IMAGE_LIBRARIES "${CMAKE_CURRENT_LIST_DIR}/lib/x64/SDL2_image.lib")
-else ()
-  set(SDL2_IMAGE_LIBRARIES "${CMAKE_CURRENT_LIST_DIR}/lib/x86/SDL2_image.lib")
-endif ()
+# vs
+if(CMAKE_SIZEOF_VOID_P EQUAL 8)
+	set(VC_LIB_PATH_SUFFIX lib/x64)
+else()
+	set(VC_LIB_PATH_SUFFIX lib/x86)
+endif()
 
-string(STRIP "${SDL2_IMAGE_LIBRARIES}" SDL2_IMAGE_LIBRARIES)
+find_library(SDL2_IMAGE_LIBRARY
+	NAMES SDL2_image
+	HINTS ${SDL2_DIR}
+	PATH_SUFFIXES lib ${VC_LIB_PATH_SUFFIX}
+)
