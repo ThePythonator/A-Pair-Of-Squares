46dd635a6f23f10ce631d5857bcccbd79951f1cd
ThePythonator <49040244+ThePythonator@users.noreply.github.com>
2021-10-08 17:49:11 +0100

    Added more functionality to AudioHandler.

--- AudioManager.cpp
+++ AudioManager.cpp
@@ -24,6 +24,112 @@ bool AudioHandler::init(int frequency, uint16_t format, uint8_t channels, int ch
 	return true;
 }
 
-uint8_t AudioHandler::play(Mix_Chunk* chunk, int loops, int channel) {
-	return Mix_PlayChannel(channel, chunk, loops);
+void AudioHandler::quit() {
+	// Quit mixer
+	Mix_Quit();
+}
+
+uint8_t AudioHandler::play_sound(Sound sample, int loops, int channel) {
+	return Mix_PlayChannel(channel, sample, loops);
+}
+
+void AudioHandler::set_sound_volume(float volume) {
+	Mix_Volume(-1, convert_volume_float_to_int(volume));
+}
+
+void AudioHandler::set_sound_volume(uint8_t channel, float volume) {
+	if (channel < channels) {
+		Mix_Volume(channel, convert_volume_float_to_int(volume));
+	}
+	else {
+		printf("Failed to set sound volume! Channel number supplied does not exist.");
+	}
+}
+
+float AudioHandler::get_sound_volume(uint8_t channel) {
+	return convert_volume_int_to_float(Mix_Volume(channel, -1));
+}
+
+Sound AudioHandler::load_sound(std::string path) {
+	// Load sample
+	Sound sample = Mix_LoadWAV(path.c_str());
+
+	if (sample == NULL) {
+		// Failed to load
+		printf("Failed to load sound sample! SDL_mixer Error: %s\n", Mix_GetError());
+	}
+
+	return sample;
+}
+
+void AudioHandler::free_sound(Sound* sample) {
+	// Free sample
+	Mix_FreeChunk(*sample);
+	*sample = NULL;
+}
+
+
+
+void AudioHandler::play_music(Music music, int loops = -1) {
+	Mix_PlayMusic(music, loops);
+}
+
+void AudioHandler::halt_music() {
+	Mix_HaltMusic();
+}
+
+bool AudioHandler::is_music_playing() {
+	return Mix_PlayingMusic();
+}
+
+bool AudioHandler::is_music_fading_in() {
+	return Mix_FadingMusic() == MIX_FADING_IN;
+}
+
+bool AudioHandler::is_music_fading_out() {
+	return Mix_FadingMusic() == MIX_FADING_OUT;
+}
+
+void AudioHandler::fade_music_in(Music music, int ms, int loops = -1) {
+	Mix_FadeInMusic(music, loops, ms);
+}
+
+void AudioHandler::fade_music_out(Music music, int ms, int loops = -1) {
+	Mix_FadeOutMusic(ms);
+}
+
+void AudioHandler::set_music_volume(float volume) {
+	Mix_VolumeMusic(convert_volume_float_to_int(volume));
+}
+
+float AudioHandler::get_music_volume() {
+	return convert_volume_int_to_float(Mix_VolumeMusic(-1));
+}
+
+Music AudioHandler::load_music(std::string path) {
+	// Load music
+	Music music = Mix_LoadMUS(path.c_str());
+
+	if (music == NULL) {
+		// Failed to load
+		printf("Failed to load music! SDL_mixer Error: %s\n", Mix_GetError());
+	}
+
+	return music;
+}
+
+void AudioHandler::free_music(Music* sample) {
+	// Free sample
+	Mix_FreeMusic(*sample);
+	*sample = NULL;
+}
+
+
+
+int AudioHandler::convert_volume_float_to_int(float volume) {
+	return static_cast<int>(volume * MIX_MAX_VOLUME);
+}
+
+float AudioHandler::convert_volume_int_to_float(int volume) {
+	return static_cast<float>(volume) / MIX_MAX_VOLUME;
 }
--- AudioManager.hpp
+++ AudioManager.hpp
@@ -4,8 +4,12 @@
 
 #include <cstdint>
 #include <cstdio>
+#include <string>
 
 #include "Constants.hpp"
+
+typedef Mix_Chunk* Sound;
+typedef Mix_Music* Music;
 
 class AudioHandler {
 public:
@@ -13,11 +17,39 @@ public:
 
 	bool init(int frequency = AUDIO::DEFAULT_FREQUENCY, uint16_t format = MIX_DEFAULT_FORMAT, uint8_t channels = AUDIO::DEFAULT_CHANNELS, int chunksize = AUDIO::DEFAULT_CHUNKSIZE);
 
+	void quit();
+
 	// todo: add loading/clearing funcs and maybe typedef Mix_Chunk
 	
-	uint8_t play(Mix_Chunk* chunk, int loops = 0, int channel = -1);
+	uint8_t play_sound(Sound sample, int loops = 0, int channel = -1);
+
+	void set_sound_volume(float volume);
+	void set_sound_volume(uint8_t channel, float volume);
+	float get_sound_volume(uint8_t channel);
+
+	static Sound load_sound(std::string path);
+	static void free_sound(Sound* sample);
+
+	void play_music(Music music, int loops = -1);
+	void halt_music();
+
+	bool is_music_playing();
+	bool is_music_fading_in();
+	bool is_music_fading_out();
+
+	void fade_music_in(Music music, int ms, int loops = -1);
+	void fade_music_out(Music music, int ms, int loops = -1);
+
+	void set_music_volume(float volume);
+	float get_music_volume();
+
+	static Music load_music(std::string path);
+	static void free_music(Music* sample);
 
 private:
+	static int convert_volume_float_to_int(float volume);
+	static float convert_volume_int_to_float(int volume);
+
 	int frequency = AUDIO::DEFAULT_FREQUENCY;
 	uint16_t format = MIX_DEFAULT_FORMAT;
 	uint8_t channels = AUDIO::DEFAULT_CHANNELS;
